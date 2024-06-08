c20d2ec573486cfdf20502f060fe0c48b6255c1e
Severus Snape <144463175+0x00snape@users.noreply.github.com>
2024-06-08 12:24:50 +0545

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,10 @@ fn main() {
     
     if pid != sysinfo::Pid::from(0) {
         println!("{}{:?} process running in PID:{}", "\t".repeat(5), env!("CARGO_PKG_NAME"), std::process::id());      
-        println!("{}PID of SSHD:{}\n", "\t".repeat(6),pid);
+        println!("{}PID of SSHD:{}", "\t".repeat(6),pid);
         
         let child_pid = modules::checkTHREAD(pid);
-        println!("{}PID of Child:{}", "\t".repeat(6), child_pid);
+        println!("{}PID of Child:{}\n", "\t".repeat(6), child_pid);
 
         // Child lookup in syscall
         modules::debugTHREAD(child_pid);
