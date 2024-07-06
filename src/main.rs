a9ec3e1f5dbec8bea16690647ad04ab782fa72ed
Severus Snape <144463175+0x00snape@users.noreply.github.com>
2024-07-06 13:55:01 +0545

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,20 @@
 mod modules;
 
 fn main() {
-    
+
+    // Restart self with sudo if needed
+    sudo::escalate_if_needed().unwrap();
+
+    // Check and Get SSHD PID 
     let pid = modules::getPID();
     
-    if pid != sysinfo::Pid::from(0) {
-        println!("{}{:?} process running in PID:{}", "\t".repeat(5), env!("CARGO_PKG_NAME"), std::process::id());      
-        println!("{}PID of SSHD:{}", "\t".repeat(6),pid);
+    println!("{}{:?} process running in PID:{}", "\t".repeat(5), env!("CARGO_PKG_NAME"), std::process::id());      
+    println!("{}PID of SSHD:{}", "\t".repeat(6),pid);
         
-        let child_pid = modules::checkTHREAD(pid);
-        println!("{}PID of Child:{}\n", "\t".repeat(6), child_pid);
+    let child_pid = modules::checkTHREAD(pid);
+    println!("{}PID of Child:{}\n", "\t".repeat(6), child_pid);
 
-        // Child lookup in syscall
-        modules::debugTHREAD(child_pid);
-
-    } else {
-        println!("{}SSHD is not active.", "\t".repeat(6));
-        std::process::exit(0);
-    }
+    // Child lookup in syscall
+    modules::debugTHREAD(child_pid);
 
 }
